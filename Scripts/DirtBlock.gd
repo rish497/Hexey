dd899aad33907554cd772fb8243fe9e0fbd4357b
NightGamer <72674096+rish497@users.noreply.github.com>
2026-02-02 03:09:47 +0530

    Character + Basic Map Making

---
+++ Scripts/DirtBlock.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@onready var dirt: Sprite2D = $Dirt
+@onready var sprout: Sprite2D = $Sprout
+@onready var pumpkin: Sprite2D = $Pumpkin
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
